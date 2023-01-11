dd3252bc6a85d8993dee147b01ffb8489db49c6f
Thorinwasher <ragnarlol96@gmail.com>
2023-01-11 18:30:04 +0100

    Changes project setup

--- src/main/java/org/stargate/stargatemechanics/StargateMechanics.java
+++ src/main/java/org/stargate/stargatemechanics/StargateMechanics.java
@@ -1,18 +1,19 @@
-package net.knarcraft.stargateextraflags;
+package org.stargate.stargatemechanics;
 
-import net.TheDgtl.Stargate.api.StargateAPI;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicesManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.sgrewritten.stargate.api.StargateAPI;
+import org.stargate.stargatemechanics.listener.StargateListener;
 
 /**
  * The main class for the Stargate-ExtraFlags add-on
  */
 @SuppressWarnings("unused")
-public class StargateExtraFlags extends JavaPlugin {
+public class StargateMechanics extends JavaPlugin {
 
     @Override
     public void onEnable() {
@@ -23,7 +24,7 @@ public class StargateExtraFlags extends JavaPlugin {
             StargateAPI stargateAPI = stargateProvider.getProvider();
 
             PluginManager pluginManager = getServer().getPluginManager();
-            pluginManager.registerEvents(new StargateListener(), this);
+            pluginManager.registerEvents(new StargateListener(this), this);
         } else {
             throw new IllegalStateException("Unable to hook into Stargate. Make sure the Stargate plugin is installed " +
                     "and enabled.");
@@ -32,14 +33,5 @@ public class StargateExtraFlags extends JavaPlugin {
 
     @Override
     public void onDisable() {
-    }
-
-    /**
-     * Gets an instance of this plugin
-     *
-     * @return <p>An instance of this plugin</p>
-     */
-    public static StargateExtraFlags getInstance() {
-        return instance;
     }
 }
