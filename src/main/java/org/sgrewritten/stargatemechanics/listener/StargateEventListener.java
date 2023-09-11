f3119cdae394b14ab79c92f752f893b001b0fb25
Thorinwasher <ragnarlol96@gmail.com>
2023-09-11 21:30:47 +0200

    Update from api change

--- src/main/java/org/sgrewritten/stargatemechanics/listener/StargateEventListener.java
+++ src/main/java/org/sgrewritten/stargatemechanics/listener/StargateEventListener.java
@@ -4,15 +4,18 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.sgrewritten.stargate.api.event.portal.StargateCreatePortalEvent;
 import org.sgrewritten.stargate.api.network.portal.PositionType;
 import org.sgrewritten.stargate.api.gate.GateAPI;
 import org.sgrewritten.stargate.api.network.RegistryAPI;
 import org.sgrewritten.stargate.api.network.portal.PortalPosition;
 import org.sgrewritten.stargate.api.network.portal.RealPortal;
-import org.sgrewritten.stargate.api.event.StargateCreateEvent;
 import org.sgrewritten.stargate.api.network.portal.PortalFlag;
 import org.sgrewritten.stargatemechanics.StargateMechanics;
+import org.sgrewritten.stargatemechanics.portal.MechanicsFlag;
+import org.sgrewritten.stargatemechanics.redstone.RedstoneEngine;
 import org.sgrewritten.stargatemechanics.utils.NoSignUtils;
+import org.sgrewritten.stargatemechanics.utils.redstone.RedstoneUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,20 +23,25 @@ import java.util.List;
 public class StargateEventListener implements Listener{
 
     private final RegistryAPI registry;
+    private final RedstoneEngine engine;
     private StargateMechanics plugin;
 
-    public StargateEventListener(StargateMechanics plugin, RegistryAPI registry) {
+    public StargateEventListener(StargateMechanics plugin, RegistryAPI registry, RedstoneEngine engine) {
         this.plugin = plugin;
         this.registry = registry;
+        this.engine = engine;
     }
     
-    @EventHandler
-    public void onStargateCreate(StargateCreateEvent event) {
-        if(!(event.getPortal() instanceof RealPortal)) {
+    @EventHandler(ignoreCancelled = true)
+    public void onStargateCreate(StargateCreatePortalEvent event) {
+        if(!(event.getPortal() instanceof RealPortal realPortal)) {
             return;
         }
         if(event.getPortal().hasFlag(PortalFlag.NO_SIGN)) {
-            NoSignUtils.removeSignsFromPortal((RealPortal) event.getPortal());
+            NoSignUtils.removeSignsFromPortal(realPortal);
+        }
+        if(event.getPortal().hasFlag(MechanicsFlag.REDSTONE_POWERED.getCharacterRepresentation())){
+            RedstoneUtils.loadPortal(realPortal, engine);
         }
     }
 }
