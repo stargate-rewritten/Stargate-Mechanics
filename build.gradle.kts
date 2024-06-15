0c6fbf3561ca4a9286dd763d48d7912b3a745779
Thorinwasher <ragnarlol96@gmail.com>
2024-06-15 19:14:55 +0200

    Debug code for Stargate-TMS

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,7 @@ tasks {
         dependencies {
             include(dependency("net.wesjd:anvilgui"))
         }
+        System.out.println("ping")
         relocate("net.wesjd.anvilgui", project.group.toString() + "." + project.name + ".anvilgui")
         archiveClassifier.set("")
     }
